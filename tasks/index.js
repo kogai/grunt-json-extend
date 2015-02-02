8dfa610d678fc0bc7a43b81de41a7aad31935ce5
小飼慎一 <kogai0121@gmail.com>
2015-02-02 10:47:10 +0900

    partion out tasks/index.js to lib/

--- tasks/index.js
+++ tasks/index.js
@@ -1,68 +1,13 @@
 "use strict";
 
-var fs = require('fs');
-var Q = require('q');
+var jsonExtend = require('../lib');
 
 module.exports = function (grunt) {
     grunt.registerTask( 'jsonExtend', 'json extend', function () {
-
         var src = grunt.config('jsonExtend').src;
         var dest = grunt.config('jsonExtend').dest;
         var callbacks = grunt.config('jsonExtend').callbacks;
         var done = this.async();
-
-        var readJson = function( obj ){
-            var d = Q.defer();
-            fs.readFile( src, function( err, data ){
-                obj = JSON.parse(data);
-                d.resolve(obj);
-            });
-            return d.promise;
-        };
-
-        var runTask = function( data ){
-            var d = Q.defer();
-            for (var x = 0; x < data.length; x++) {
-                data[x];
-
-                var arr = [];
-                for (var i = 0; i < callbacks.length; i++ ) {
-                    var result = callbacks[i]( data[x] );
-                    if( typeof result !== 'object' ){
-                        throw '\nCallback function expect return the object.\nReturn value is ' + result ;
-                    }
-                    arr.push(result);
-                }
-
-                for (var i = 0; i < arr.length; i++) {
-                    var obj = arr[i];
-                    for( var key in obj ){
-                        data[x][key] = obj[key];
-                    }
-                }
-
-            }
-            d.resolve(data);
-            return d.promise;
-        };
-
-        var writeJson = function( data ){
-            var obj = JSON.stringify(data, null, 4);
-            fs.writeFile( dest, obj, function( err ){
-                if( err ) throw err;
-                console.log('json file saved.');
-                done();
-            })
-        };
-
-        var hundleJson = function(){
-            Q.when([])
-            .then(readJson)
-            .then(runTask)
-            .done(writeJson);
-        };
-
-        hundleJson();
-
+        jsonExtend( src, dest, callbacks, done );
     });
 };
